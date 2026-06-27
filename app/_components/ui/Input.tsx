68b9140ca0f9bf080c6233b5ff96fa5d64a6ee1a
Kawagoe53 <y.kawagoe1231@gmail.com>
2026-06-27 22:51:14 +0900

    fix:表示崩れの修正

--- app/_components/ui/Input.tsx
+++ app/_components/ui/Input.tsx
@@ -19,7 +19,11 @@ export default function Input({
   return (
     <div>
       {label && <Label htmlFor={props.id}>{label}</Label>}
-      <input {...registration} {...props} />
+      <input
+        {...registration}
+        {...props}
+        className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+      />
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
     </div>
   );
