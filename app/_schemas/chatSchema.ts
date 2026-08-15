2f486c8d8dabc9348a0294773aefb79ebc3eb440
Kawagoe53 <y.kawagoe1231@gmail.com>
2026-08-15 11:06:40 +0900

    feature:chatSchema.ts被ってたので削除

--- app/_schemas/chatSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-
-// バリデーションスキーマの定義
-export const chatSchema = z.object({
-  message: z.string().min(1, { message: "文字を入力してください" }),
-});
-
-// 型定義のエクスポート
-export type ChatSchemaValues = z.infer<typeof chatSchema>;
